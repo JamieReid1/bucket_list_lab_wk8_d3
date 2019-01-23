b7154af53ccd66d067461c1e44db18e7b4b388b4
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-23 14:16:24 +0000

    Sets up create new item and delete item routes.

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -6,6 +6,7 @@ const CreateRouter = function(collection) {
 
   const router = express.Router();
 
+  //SHOW
   router.get('/', (req, res) => {
     collection
       .find()
@@ -17,7 +18,7 @@ const CreateRouter = function(collection) {
         res.json({ status: 500, error: err });
       });
   });
-
+  //SHOW BY ID
   router.get('/:id', (req,res) => {
     const id = req.params.id;
     collection
@@ -28,6 +29,32 @@ const CreateRouter = function(collection) {
         res.status(500);
         res.json({ status: 500, error: err });
       });
+  });
+  // CREATE
+  router.post('/', (req, res) => {
+      const newItem = req.body;
+      collection
+      .insertOne(newItem)
+      .then(() => collection.find().toArray())
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+  // DESTROY
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+      collection
+      .deleteOne({_id: ObjectID(id)})
+      .then(() => collection.find().toArray())
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   });
 
   return router;
