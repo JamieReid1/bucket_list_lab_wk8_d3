7940f1fc4193ed2248f4fed37d099616d01ed099
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-23 13:56:41 +0000

    Sets up backend get index route.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
+const CreateRouter = require('./helpers/create_router.js');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
@@ -11,7 +11,7 @@ MongoClient.connect('mongodb://localhost:27017')
   .then((client) => {
     const db = client.db('bucket_list');
     const collection = db.collection('bucketListItems');
-    const router = createRouter(collection);
+    const router = CreateRouter(collection);
     app.use('/api/items', router);
   })
   .catch(console.err);
